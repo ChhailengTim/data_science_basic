5bdfe33b48aa4090714c20cd97ffc7f4e1fbcd56
CHHAILENG TIM <chhailengtim2018@gmail.com>
2025-04-23 23:27:21 +0700

    practice

--- Lab1/lab1_ex7.py
+++ Lab1/lab1_ex7.py
@@ -1,8 +1,13 @@
 num = eval(input("Enter a number : "))
 
-if num / 5 == 0 and num / 6 == 0:
-    print(True)
-elif num / 5 == 0 or num / 6 == 0:  
-    print(True)
-else:
-    print(False)
+divide_by_5 = (num % 5 == 0)
+divide_by_6 = (num % 6 == 0)
+
+both = divide_by_5 and divide_by_6
+either = divide_by_5 or divide_by_6
+only_one = divide_by_5 != divide_by_6
+
+# Print the results
+print(f"Is {num} divisible by 5 and 6? {both}")
+print(f"Is {num} divisible by 5 or 6? {either}")
+print(f"Is {num} divisible by 5 or 6, but not both? {only_one}")
